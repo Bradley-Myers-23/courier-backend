6ee5f79546a2233c9e5e0b69271bce1269c9a7ee
Bradley-Myers-23 <bradley.myers@eagles.oc.edu>
2023-06-22 17:30:02 -0500

    first commit

---
+++ app/routes/ingredient.routes.js
@@ -0,0 +1,25 @@
+module.exports = (app) => {
+  const Ingredient = require("../controllers/ingredient.controller.js");
+  var router = require("express").Router();
+  const { authenticateRoute } = require("../authentication/authentication");
+
+  // Create a new Ingredient
+  router.post("/ingredients/",Ingredient.create);
+
+  // Retrieve all Ingredient
+  router.get("/ingredients/", Ingredient.findAll);
+
+  // Retrieve a single Ingredient with ingredientId
+  router.get("/ingredients/:id", Ingredient.findOne);
+
+  // Update an Ingredient with ingredientId
+  router.put("/ingredients/:id",Ingredient.update);
+
+  // Delete an Ingredient with ingredientId
+  router.delete("/ingredients/:id",Ingredient.delete);
+
+  // Create a new Ingredient
+  router.delete("/ingredients/",Ingredient.deleteAll);
+
+  app.use("/travelapi", router);
+};
