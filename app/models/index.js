f85be02d9ee8869540e1b4e60e838d86a96e6c7a
niroshanperera <niroshan.perera@eagles.oc.edu>
2023-07-05 23:49:56 -0500

    Update index.js
    resolved merge conflict

--- app/models/index.js
+++ app/models/index.js
@@ -24,6 +24,8 @@ db.tripDay = require("./tripDay.model.js")(sequelize, Sequelize);
 db.userTrip = require("./userTrip.model.js")(sequelize, Sequelize);
 db.hotelDay = require("./hotelDay.model.js")(sequelize, Sequelize);
 db.order = require("./order.model.js")(sequelize, Sequelize);
+db.rate = require("./rate.model.js")(sequelize, Sequelize);
+db.customer = require("./customer.model.js")(sequelize, Sequelize);
 db.mapData = require("./mapData.model.js")(sequelize, Sequelize);
 // foreign keys for tripSite
 db.tripDay.hasMany(
